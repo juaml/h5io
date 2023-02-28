703521280067299a19e4628bdc878f00855495de
Synchon Mandal <synchon@protonmail.com>
2023-02-28 13:05:31 +0100

    update: explicit support for 2D and 3D data in ChunkedArray

--- h5io/chunked_array.py
+++ h5io/chunked_array.py
@@ -4,36 +4,36 @@
 #          Federico Raimondo <f.raimondo@fz-juelich.de>
 # License: BSD (3-clause)
 
-from typing import Tuple
+from typing import Tuple, Union
 
 import numpy as np
 
 
 class ChunkedArray:
-    """Class for chunked numpy.ndarray.
+    """Class for chunked 2D and 3D numpy.ndarray.
 
     Parameters
     ----------
-    data : numpy.ndarray
+    data : 2D or 3D numpy.ndarray
         The data to be stored in chunks.
     shape : tuple of int
-        A 3-tuple of int for (x, y, t).
+        A tuple of int for (x, t) or triple of int for (x, y, t).
     chunk_size : tuple of int
-        The 3-tuple of int as chunk size of chunked storage.
+        The tuple or triple of int as chunk size of chunked storage.
     n_chunk : int
         The chunk count of chunked storage.
 
     Notes
     -----
-    The first two dimensions of the ``chunk_size`` and the ``shape``
+    The first one or two dimensions of the ``chunk_size`` and the ``shape``
     must be the same.
 
     """
     def __init__(
         self,
         data: np.ndarray,
-        shape: Tuple[int, int, int],
-        chunk_size: Tuple[int, int, int],
+        shape: Union[Tuple[int, int], Tuple[int, int, int]],
+        chunk_size: Union[Tuple[int, int], Tuple[int, int, int]],
         n_chunk: int,
     ) -> None:
         self.chunkdata = data
@@ -41,29 +41,53 @@ class ChunkedArray:
         self.chunk_size = chunk_size
         self.n_chunk = n_chunk
 
-        self._chunk_start = self.n_chunk * self.chunk_size[2]
-        self._chunk_end = (self.n_chunk + 1) * self.chunk_size[2]
-        if self._chunk_end > self.shape[2]:
-            self._chunk_end = self.shape[2]
+        if self.chunkdata.ndim == 2:
+            self._chunk_start = self.n_chunk * self.chunk_size[1]
+            self._chunk_end = (self.n_chunk + 1) * self.chunk_size[1]
+            if self._chunk_end > self.shape[1]:
+                self._chunk_end = self.shape[1]
 
-        if self._chunk_start > self.shape[2]:
-            raise ValueError(
-                f"The chunk number ({n_chunk}) is too large for the given "
-                f"shape ({shape})."
-            )
-        t_chunk_size = self._chunk_end - self._chunk_start
-        if t_chunk_size != self.chunkdata.shape[2]:
-            raise ValueError(
-                f"The chunk size ({chunk_size}) does not match the given "
-                f"data shape ({self.chunkdata.shape})."
-            )
+            if self._chunk_start > self.shape[1]:
+                raise ValueError(
+                    f"The chunk number ({n_chunk}) is too large for the given "
+                    f"shape ({shape})."
+                )
+            t_chunk_size = self._chunk_end - self._chunk_start
+            if t_chunk_size != self.chunkdata.shape[1]:
+                raise ValueError(
+                    f"The chunk size ({chunk_size}) does not match the given "
+                    f"data shape ({self.chunkdata.shape})."
+                )
+        elif self.chunkdata.ndim == 3:
+            self._chunk_start = self.n_chunk * self.chunk_size[2]
+            self._chunk_end = (self.n_chunk + 1) * self.chunk_size[2]
+            if self._chunk_end > self.shape[2]:
+                self._chunk_end = self.shape[2]
+
+            if self._chunk_start > self.shape[2]:
+                raise ValueError(
+                    f"The chunk number ({n_chunk}) is too large for the given "
+                    f"shape ({shape})."
+                )
+            t_chunk_size = self._chunk_end - self._chunk_start
+            if t_chunk_size != self.chunkdata.shape[2]:
+                raise ValueError(
+                    f"The chunk size ({chunk_size}) does not match the given "
+                    f"data shape ({self.chunkdata.shape})."
+                )
 
     @property
     def chunkslice(self) -> Tuple[slice, slice, slice]:
         """Return the slice of the chunk to write to the dataset."""
-        out = (
-            slice(0, self.shape[0], 1),
-            slice(0, self.shape[1], 1),
-            slice(self._chunk_start, self._chunk_end, 1),
-        )
+        if self.chunkdata.ndim == 2:
+            out = (
+                slice(0, self.shape[0], 1),
+                slice(self._chunk_start, self._chunk_end, 1),
+            )
+        elif self.chunkdata.ndim == 3:
+            out = (
+                slice(0, self.shape[0], 1),
+                slice(0, self.shape[1], 1),
+                slice(self._chunk_start, self._chunk_end, 1),
+            )
         return out
